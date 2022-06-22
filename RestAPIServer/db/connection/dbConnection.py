96d9326d967168ee449b5569558ea2b9922b6405
4kang0624 <4kang0624@naver.com>
2022-06-22 21:08:29 +0900

    feat: Document 데이터베이스 기능 추가
    기능 추가
     - 데이터 입력,
     - db 안의 모든 데이터 출력,
     - 특정 문서의 가장 마지막 버전 데이터 출력,
     - 가장 마지막 버전 출력

--- RestAPIServer/db/connection/dbConnection.py
+++ RestAPIServer/db/connection/dbConnection.py
@@ -2,14 +2,14 @@ from pymongo import MongoClient
 
 
 # MongoDB 클라이언트 생성
-def getDbClient(url='localhost', port=27017):
+def getClient(url='localhost', port=27017):
     client = MongoClient(url, port)
     print('MongoDB Connected')
     return client
 
 
 # MongoDB 데이터베이스 생성
-def getDb(dbName, client=getDbClient()):
+def getDb(dbName, client=getClient()):
     db = client[dbName]
     return db
 
