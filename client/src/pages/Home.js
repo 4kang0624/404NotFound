b0c36be15bdc8a518ee39b55bb16374edce8e703
kadilan <yes235817@gmail.com>
2022-06-24 03:43:01 +0900

    fix: Home 수정

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,11 +5,40 @@ import {
   IconButton,
   Chip,
   Grid,
+  ImageList,
+  ImageListItem,
+  ImageListItemBar,
+  ListSubheader,
 } from "@mui/material";
+
 import SearchIcon from "@mui/icons-material/Search";
 import { Box } from "@mui/system";
 import React, { useState } from "react";
 import SearchResult from "../components/SearchResult";
+import InfoIcon from "@mui/icons-material/Info";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+const itemData = [
+  {
+    img: "https://miro.medium.com/max/1400/0*5CnyWf0j4a9daNkY",
+    title: "Python",
+    version: 2,
+    src: "/signin",
+  },
+  {
+    img: "https://miro.medium.com/max/1400/0*i4vAu1uZ1-N06EA-.jpg",
+    title: "ComputerArchitecture",
+    version: 1,
+    src: "/signin",
+  },
+  {
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/140px-Unofficial_JavaScript_logo_2.svg.png",
+    title: "JavaScript",
+    version: 1,
+    src: "/signin",
+  },
+];
 
 function Home() {
   const [isCard, setIsCard] = useState(false);
@@ -24,7 +53,9 @@ function Home() {
       >
         코드위키
       </Typography>
+
       <Paper
+        variant="outlined"
         component="form"
         sx={{
           p: "2px 4px",
@@ -32,21 +63,23 @@ function Home() {
           alignItems: "center",
           width: 400,
           height: 40,
-          borderRadius: "20px",
-          bgcolor: "black",
+          border: "1px solid gray",
+          outlineColor: "white",
+          bgcolor: "white",
+          boxShadow: 0,
         }}
       >
         <InputBase
-          sx={{ ml: 1, flex: 1, color: "white" }}
+          sx={{ ml: 2, flex: 1, color: "black" }}
           // placeholder="Search Google Maps"
           inputProps={{ "aria-label": "search document" }}
         />
         <IconButton type="submit" sx={{ p: "10px" }} aria-label="search">
-          <SearchIcon sx={{ color: "white" }} />
+          <SearchIcon sx={{ color: "black" }} />
         </IconButton>
       </Paper>
-      <Grid container sx={{ maxWidth: "900px", marginTop: "30px" }}>
-        <Grid item xs={3} textAlign={"center"}>
+      {/* <Grid container sx={{ maxWidth: "500px", marginTop: "30px", ml: 11 }}>
+        <Grid item xs={2} textAlign={"center"}>
           <Chip
             label="학생"
             variant="outlined"
@@ -67,15 +100,45 @@ function Home() {
             onClick={() => console.log("dd")}
           />
         </Grid>
-        <Grid item xs={3} textAlign={"center"}>
+        <Grid item xs={2} textAlign={"center"}>
           <Chip
             label="전체"
             variant="outlined"
             onClick={() => console.log("dd")}
           />
         </Grid>
-      </Grid>
+      </Grid> */}
       {isCard && <SearchResult />}
+      <ImageList
+        sx={{ width: 500, height: 450, marginTop: "45px" }}
+        cols={3}
+        rowHeight={164}
+      >
+        {itemData.map(item => (
+          <Link to={item.src}>
+            <ImageListItem key={item.img}>
+              <img
+                src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={item.title}
+                loading="lazy"
+              />
+              <ImageListItemBar
+                title={item.title}
+                subtitle={"Ver. " + item.version}
+                actionIcon={
+                  <IconButton
+                    sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                    aria-label={"info about ${item.title}"}
+                  >
+                    <InfoIcon />
+                  </IconButton>
+                }
+              ></ImageListItemBar>
+            </ImageListItem>
+          </Link>
+        ))}
+      </ImageList>
     </Box>
   );
 }
